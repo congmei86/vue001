1c0ecd8159002ef21ffafcc74efae3b349fe1ad3
buouniao <cong.mei86@gmail.com>
2020-12-12 22:52:07 +0100

    my first vue project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app-container">
+    <!-- 顶部 Header 区部 -->
+    <mt-header fixed title="葱头的Vue项目"></mt-header>
+
+    <!-- 中间的 路由 router-view 区部 -->
+
+
+    <!-- 底部 Tabbar 区部 -->
+
+<h1>123</h1>
+  </div>
+</template>
+
+<script>
+</script>
+
+
+<style lang="scss" scoped>
+.app-container{
+  padding-top:  40px;
+}
+</style>
